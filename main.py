1183a2c6f85688dd3e8bcaa0021834d26189586f
Anatoly Belikov <awbelikov@gmail.com>
2019-08-05 13:32:21 +0300

    slow storage emulation

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import uuid
+from collections import defaultdict
 from opencog.atomspace import AtomSpace, types, create_child_atomspace
 from opencog.utilities import initialize_opencog, tmp_atomspace
 from opencog.type_constructors import *
 from opencog.scheme_wrapper import scheme_eval_h
 from opencog.bindlink import execute_atom
+import time
 
 
 def replace(atom, semantic_space, pattern_space, varmapping):
@@ -34,13 +36,46 @@ def rename(tmp, atomspace, atom):
     return atom
 
 
+class Storage:
+    def __init__(self):
+        self._hash_to_idx = defaultdict(list)
+        self._idx_to_pos = dict()
+        self.slow_storage = list()
+
+    def add_atom(self, atom):
+        id = self.find_id_by_atom(atom)
+        new = False
+        if id < 0:
+            id = len(self.slow_storage)
+            time.sleep(1)
+            self.slow_storage.append(str(atom))
+            self._hash_to_idx[hash(atom)].append(id)
+            new = True
+        return id, new
+
+    def find_id_by_atom(self, atom):
+        ids = self._hash_to_idx.get(hash(atom), [])
+        if not ids:
+            return -1
+        atoms = self.get_atoms(ids)
+        for (idx, a) in zip(ids, atoms):
+            if atom == a:
+                return idx
+        return -1
+
+    def get_atoms(self, ids):
+        time.sleep(1)
+        result = [self.slow_storage[i] for i in ids]
+        return result
+
 
 class Index:
-    def __init__(self):
+    def __init__(self, storage:Storage):
         self.pattern_space = AtomSpace()
         self.semantic_space = AtomSpace()
-        self.data_space = AtomSpace()
-        # index maps subgraph pattern to maching atoms
+        # storage maps index to atom
+        self._storage = storage
+        # index maps subgraph pattern to matching atoms
         self._index = dict()
 
     def add_toplevel_pattern(self, atom):
@@ -53,30 +88,29 @@ class Index:
     def add_semantic_pattern(self, atom):
         # find all nodes, that inherit from some atom from semantic atomspace
         # replace such atoms by variable nodes
-
         result = replace(atom, self.semantic_space, self.pattern_space, dict())
         pattern = self.pattern_space.add_link(types.BindLink, [result, result])
         self.add_pattern(pattern)
 
     def add_data(self, data):
-        data = self.data_space.add_atom(data)
+        idx, new = self._storage.add_atom(data)
+        if not new:
+            return
         assert data.is_link()
         # self.add_toplevel_pattern(data)
         self.add_semantic_pattern(data)
 
         tmp = create_child_atomspace(self.pattern_space)
         content = tmp.add_atom(data)
-        self.update_index(tmp, content, data)
+        self.update_index(tmp, content, idx)
 
-    def update_index(self, atomspace, content, data):
+    def update_index(self, atomspace, data, idx):
         for pat in self._index.keys():
             match = execute_atom(atomspace, pat)
             # patterns can match patterns, so check:
             for m in match.out:
-                # should work with atoms from different atomspaces after
-                # merge, then it is possible to remove content
-                if hash(m) == hash(content):
-                    self._index[pat].add(data)
+                if m == data:
+                    self._index[pat].add(idx)
 
     def intersect(self, query):
         """
@@ -86,7 +120,7 @@ class Index:
         # put query in tmp atomspace, check if there are relevant patterns
         tmp = create_child_atomspace(self.pattern_space)
         q = tmp.add_atom(rename(tmp, self.pattern_space, query))
-        # check for exact match:
+        # check query for exact match:
         exact_match = execute_atom(tmp, tmp.add_link(types.BindLink, [q, q]))
         for m in exact_match.out:
             return self._index[self.pattern_space.add_link(types.BindLink, [m, m])]
@@ -103,16 +137,21 @@ class Index:
                         res_set = idx.intersection(idx)
         return res_set
 
-    def query(self, query):
+    def query(self, result_atomspace, query):
         res_set = self.intersect(query)
         # add all possible results to tmp atomspace and run query
         # what is called filter step in graph indexing literature
         tmp = AtomSpace()
-        for item in res_set:
-            tmp.add_atom(item)
+        atom_str = self._storage.get_atoms(res_set)
+        for item in atom_str:
+            tmp.add_atom(scheme_eval_h(tmp, item))
         # pack query in BindLink
         q = tmp.add_link(types.BindLink, [query, query])
-        return execute_atom(tmp, q)
+        results = execute_atom(tmp, q)
+        result_atoms = set()
+        for atom in results.out:
+            result_atoms.add(result_atomspace.add_atom(atom))
+        return result_atoms
 
     def update_index_dual_link(self, content):
         # doesn't work
@@ -129,7 +168,6 @@ class Index:
 
     def add_pattern(self, pat):
         assert pat.type == types.BindLink
-        print(hash(pat))
         atom = self.pattern_space.add_atom(pat)
         if atom not in self._index:
             self._index[atom] = set()
@@ -140,7 +178,8 @@ def main():
     path_data = 'data.scm'
     atomspace = AtomSpace()
     initialize_opencog(atomspace)
-    index = Index()
+    storage = Storage()
+    index = Index(storage)
     with tmp_atomspace() as tmp:
         imp = ImplicationLink(
                 VariableNode("C"),
@@ -148,15 +187,15 @@ def main():
         # GetLink returns only bindings for variables
         pat = BindLink(imp, imp)
         index.add_pattern(pat)
+
     with tmp_atomspace() as tmp:
         base = open(path_data).read()
         data = scheme_eval_h(tmp, base)
         index.add_data(data)
         query = open(path_query).read()
         q = scheme_eval_h(tmp, query)
-        result = index.query(q)
+        result = index.query(tmp, q)
         print(result)
 
 if __name__ == '__main__':
     main()
-
